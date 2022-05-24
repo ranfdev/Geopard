c89a59c69274d6c152fa18646b20dc9ee93f4ae0
ranfdev <ranfdev@gmail.com>
2022-05-24 13:27:29 +0200

    Update nix derivation for blueprint-compiler

--- nix/blueprint-compiler.nix
+++ nix/blueprint-compiler.nix
@@ -1,53 +1,45 @@
 { python3
-, python3Packages
 , stdenv
-, fetchgit
+, fetchFromGitLab
+, gobject-introspection
+, lib
 , meson
 , ninja
-, gtk4
-, glib
-, gobject-introspection
-, libadwaita
-, wrapGAppsHook4
 }:
-python3Packages.buildPythonApplication {
+stdenv.mkDerivation rec {
   pname = "blueprint-compiler";
-  version = "0.1.0";
+  version = "0.pre+date=2022-05-17";
 
-  src = fetchgit {
-    url = "https://gitlab.gnome.org/jwestman/blueprint-compiler.git";
-    rev = "db2e662d3173f6348b86d2350d4e2f0340ec939c";
-    sha256 = "sha256-aFx+aUDpc9wNgG/NUyIDgSZnURFShyTH1HhDjElTApY=";
+  src = fetchFromGitLab {
+    domain = "gitlab.gnome.org";
+    owner = "jwestman";
+    repo = pname;
+    rev = "06278a64248cec92bb95a958eadfba453943c061";
+    sha256 = "sha256-ukvqWvYl4NohKEm6W50eRZ9fmKD4vhH0yzgrCf5HVts=";
   };
 
-  preBuild = ''
-    cat >setup.py <<'EOF'
-    from setuptools import setup
-    setup(
-      name='blueprint-compiler',
-      version='0.1.0',
-      scripts=[
-        "blueprint-compiler.py",
-      ],
-    )
-    EOF
-  '';
+  # Requires pythonfuzz, which I've found difficult to package
   doCheck = false;
-  postInstall = ''
-    mv -v $out/bin/blueprint-compiler.py $out/bin/blueprint-compiler
-  '';
+
   nativeBuildInputs = [
-    wrapGAppsHook4
-  ];
-  buildInputs = [
-    gtk4
-    glib
-    gobject-introspection
-    python3Packages.pygobject3
-    libadwaita
-    wrapGAppsHook4
+    meson
+    ninja
+    python3.pkgs.wrapPython
   ];
   propagatedBuildInputs = [
     gobject-introspection
+    python3
   ];
+
+  postFixup = ''
+    wrapPythonPrograms
+  '';
+
+  meta = with lib; {
+    description = "A markup language for GTK user interface files";
+    homepage = "https://gitlab.gnome.org/jwestman/blueprint-compiler";
+    license = licenses.lgpl3Plus;
+    maintainers = [ maintainers.ranfdev ];
+    platforms = platforms.all;
+  };
 }
