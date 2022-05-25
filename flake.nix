a780926a68e773d96544a559a51b9befdfeccd87
ranfdev <ranfdev@gmail.com>
2022-05-25 17:07:55 +0200

    Use flake-utils

--- flake.nix
+++ flake.nix
@@ -3,43 +3,48 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     naersk.url = "github:nix-community/naersk";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, naersk }:
-    with import nixpkgs { system = "x86_64-linux"; };
-    rec {
-      packages.x86_64-linux.blueprint-compiler = callPackage ./nix/blueprint-compiler.nix { };
-      packages.x86_64-linux.geopard = callPackage ./nix/geopard.nix {
-        naersk-lib = naersk.lib.x86_64-linux;
-        blueprint-compiler = packages.x86_64-linux.blueprint-compiler;
-      };
-      checks.x86_64-linux.default = stdenv.mkDerivation {
-        name = "geopard-checks";
-        src = ./.;
-        cargoDeps = packages.x86_64-linux.geopard.cargoDeps;
-        configurePhase = ''
-          # These are replaced during the real build by meson
-          sed \
-            -e 's/str =.*;/str = "";/g' \
-            -e 's/i32 =.*;/i32 = 0;/g' \
-            src/build_config.rs.in \
-            > src/build_config.rs
-        '';
-	checkPhase = ''
-	  cargo fmt --check;
-	  cargo clippy -- -D warnings
-	'';
-	doCheck = true;
-	installPhase = ''echo "" > $out'';
-	nativeBuildInputs = with nixpkgs.legacyPackages.x86_64-linux; [openssl pkg-config glib gtk4 libadwaita clippy];
-	buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
-	  clippy
-	  rustPlatform.rust.cargo
-	  rustPlatform.rust.rustc
-	  rustPlatform.cargoSetupHook
-	  rustfmt
-	];
-      };
-      packages.x86_64-linux.build-flatpak = callPackage ./nix/build-flatpak.nix { };
-      packages.x86_64-linux.default = packages.x86_64-linux.geopard;
-    };
+  outputs = { self, nixpkgs, naersk, flake-utils }:
+    flake-utils.lib.eachSystem
+      (with flake-utils.lib.system; [ x86_64-linux aarch64-linux ])
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        rec {
+          packages.blueprint-compiler = pkgs.callPackage ./nix/blueprint-compiler.nix { };
+          packages.geopard = pkgs.callPackage ./nix/geopard.nix {
+            naersk-lib = naersk.lib.${system};
+            blueprint-compiler = packages.blueprint-compiler;
+          };
+          checks.default = pkgs.stdenv.mkDerivation {
+            name = "geopard-checks";
+            src = ./.;
+            cargoDeps = packages.geopard.cargoDeps;
+            configurePhase = ''
+              # These are replaced during the real build by meson
+              sed \
+                -e 's/str =.*;/str = "";/g' \
+                -e 's/i32 =.*;/i32 = 0;/g' \
+                src/build_config.rs.in \
+                > src/build_config.rs
+            '';
+            checkPhase = ''
+              cargo fmt --check;
+              cargo clippy -- -D warnings
+          	'';
+            doCheck = true;
+            installPhase = ''echo "" > $out'';
+            nativeBuildInputs = with pkgs; [ openssl pkg-config glib gtk4 libadwaita clippy ];
+            buildInputs = with pkgs; [
+              clippy
+              rustPlatform.rust.cargo
+              rustPlatform.rust.rustc
+              rustPlatform.cargoSetupHook
+              rustfmt
+            ];
+          };
+          packages.build-flatpak = pkgs.callPackage ./nix/build-flatpak.nix { };
+          packages.default = packages.geopard;
+        }
+      );
 }
--- nix/default.nix
+++
@@ -1,4 +0,0 @@
-{pkgs ? import <nixpkgs> {}}:
-let
-  geopard = pkgs.callPackage ./geopard.nix {};
-in geopard
